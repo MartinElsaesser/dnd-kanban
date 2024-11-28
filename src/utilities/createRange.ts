5208a323124a3253b9cd30753ab1f9347fe782a9
Martin Elsaesser <marels@aon.at>
2024-11-28 18:07:25 +0100

    correct type error

--- src/utilities/createRange.ts
+++ src/utilities/createRange.ts
@@ -2,7 +2,8 @@ const defaultInitializer = (index: number) => index;
 
 export function createRange<T = number>(
   length: number,
-  initializer: (index: number) => any = defaultInitializer
+  initializer: (index: number) => T
+    = defaultInitializer as (index:number) => T
 ): T[] {
   return [...new Array(length)].map((_, index) => initializer(index));
 }
